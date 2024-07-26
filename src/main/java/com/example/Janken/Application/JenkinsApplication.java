7f69c7872c80cb3556d8f9f9c379bcd82f42a8b4
Ravi <ravitsurvase@gmail.com>
2024-07-26 16:09:48 +0530

    Added  testing file.

--- src/main/java/com/example/Janken/Application/JenkinsApplication.java
+++ src/main/java/com/example/Janken/Application/JenkinsApplication.java
@@ -1,14 +1,17 @@
 package com.example.Janken.Application;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-public class JankenApplication {
+import java.util.logging.Logger;
 
+@SpringBootApplication
+public class JenkinsApplication {
+//    public static Logger logger=  LoggerFactory.getLogger(JenkinsApplication.class);
 	public static void main(String[] args) {
-		SpringApplication.run(JankenApplication.class, args);
+		SpringApplication.run(JenkinsApplication.class, args);
 	}
 
 	@RestController
---
+++ src/test/java/com/example/Janken/Application/JenkinsApplicationTests.java
@@ -0,0 +1,21 @@
+package com.example.Janken.Application;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.logging.Logger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class JenkinsApplicationTests {
+	public static Logger logger= (Logger) LoggerFactory.getLogger(JenkinsApplicationTests.class);
+
+	@Test
+	void contextLoads() {
+		logger.info("Test case executing....");
+		assertEquals(true,true);
+	}
+
+}
